3a4190c2a28d6c7e8631cdda19566dbafc41ccd7
imJoshuaRice <134961055+imJoshuaRice@users.noreply.github.com>
2024-04-05 20:02:46 +0100

    Create MathUtils.java

---
+++ MathUtils.java
@@ -0,0 +1,26 @@
+public class MathUtils {
+    // Method to add two integers
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    // Method to multiply two integers
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    // Method to greet a person by name
+    public void greet(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+
+    // Method to find the maximum of two integers
+    public int max(int a, int b) {
+        return (a > b) ? a : b;
+    }
+
+    // Method to find the minimum of two integers
+    public int min(int a, int b) {
+        return (a < b) ? a : b;
+    }
+}
