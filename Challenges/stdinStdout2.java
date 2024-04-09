751c404b296c66375e038647ce38b389305c9b27
imJoshuaRice <134961055+imJoshuaRice@users.noreply.github.com>
2024-04-09 19:13:48 +0100

    Solve stdinStdout2 'challenge'

--- Challenges/stdinStdout2.java
+++ Challenges/stdinStdout2.java
@@ -17,9 +17,12 @@ public class stdinStdout2 {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int i = scan.nextInt();
 
-        // Write your code here.
+        int i = Integer.parseInt(scan.nextLine());
+        Double d = Double.parseDouble(scan.nextLine());
+        String s = scan.nextLine();
+
+        scan.close();
 
         System.out.println("String: " + s);
         System.out.println("Double: " + d);
